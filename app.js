62ef148e3dea1ea8e784b523411e892061bd8939
Prarabdhshuklaa <prarabdh2001@gmail.com>
2024-11-11 13:48:39 +0530

    hello

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const upload = require("express-fileupload")
 
 //This foldeer is used for live server
 const root = require("path").join(__dirname, "dist");
+// const root = require("path").join(__dirname, "dist");
 app.use(express.static(root));
 //-----------------------LIVE SERVER---------------------------------------------
 
 
 
-app.use(express.static(__dirname+"/assets"));
+app.use(express.static(__dirname + "/assets"));
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
@@ -21,8 +22,8 @@ app.use(require("./allRoutes/routes"));
 
 
 //-----------------------LIVE SERVER below---------------------------------------------
-app.get("*" , (req,res)=>{
-    res.sendFile("index.html", {root});
+app.get("*", (req, res) => {
+    res.sendFile("index.html", { root });
 })
 //-----------------------LIVE SERVER upper---------------------------------------------
 
